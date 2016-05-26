61d4e8256241a5c5c03ba9363d4fc033d6c87fc8
Claudio Leonel Daruich <leodaruich2@gmail.com>
2016-05-26 20:55:56 -0300

    se sube archivo.py #2

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+#Esto es un programa pueba para el parcial 
+#Si es multiplo de 3 muestra FISS
+#Si es de 5 muestra buss
+#si es de ambos fissbuzz
+#si no muestra el numero
+
+#pseudo codigo  DESIGN
+
+"""Repito numeros del 1 al 100 comenzando desde 1 si es multiplo de 3, es decir que el resto debe ser 0 muestra la FIZZ, Si es multiplo de 5 y el resto da 0 muestra BUZZ, Si es multiplo de ambos muestra FIZZBUZZ, Si no es multiplo de ninguno muestra el numero """
+
