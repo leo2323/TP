3977fca87130ee9acc8a9985b229df45947ca37f
Claudio Leonel Daruich <leodaruich2@gmail.com>
2016-06-02 19:07:44 -0300

    resubida del progarama corrigiendo el error de checking #5

--- fizzbuzz2.py
+++ fizzbuzz2.py
@@ -1,26 +1,24 @@
-#Esto es un programa pueba para el parcial 
 #Si es multiplo de 3 muestra FISS
 #Si es de 5 muestra buss
 #si es de ambos fissbuzz
 #si no muestra el numero
-#version2
 
 #pseudo codigo  DESIGN
 
-"""Repito numeros del 1 al 100 comenzando desde 1 si es multiplo de 3, 
-es decir que el resto debe ser 0 muestra la FIZZ, Si es multiplo de 5 y el resto da
- 0 muestra BUZZ, Si es multiplo de ambos muestra FIZZBUZZ, Si no es multiplo de
-  ninguno muestra el numero """
+#Repito numeros del 1 al 100 comenzando desde 1 si es multiplo de 3, 
+#es decir que el resto debe ser 0 muestra la FIZZ, Si es multiplo de 5 y el resto da
+ #0 muestra BUZZ, Si es multiplo de ambos muestra FIZZBUZZ, Si no es multiplo de
+  #ninguno muestra el numero """
   
 a=1
 while a<=15 :
     if a%3 == 0 and a% 5 ==0:
         print "FizzBuzz"
     elif a%3  == 0:
-        print "Fizz"/
+        print "Fizz"
       
     elif a% 5 == 0 :
-        print "Buzz"/n
+        print "Buzz"
     
     
     else  :
