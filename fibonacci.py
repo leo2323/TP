fe60d6006c8900719e58c4bcdf9632c005b8ff72
Claudio Leonel Daruich <leodaruich2@gmail.com>
2016-06-08 01:10:04 -0300

    se sube el pseudo codigo del programa #7

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,10 @@
 #Esto es una prueba para el parcial
 #Consiste en un programa que sume 
 #los dos numeros anteriores hasta 1,000,000(un millon)
-
+# Pseudo codigo
+#es crear una lista con un valor adentro de 0
+fibonacci = [0]
+#crear dos variables que sera x ,y que tendran valor de 0 y 1
+#otra variable con la catidad de sumas a realizar
+x = 1
+y = 0
